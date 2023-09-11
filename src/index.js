1e2452d8d1a57f1528368d46969ae813cdcfd398
aishengyu <134182371+aishengyu@users.noreply.github.com>
2023-09-11 05:40:15 +0000

    https://refactored-cod-qwg6447jpxgcw77.github.dev/

--- src/index.js
+++ src/index.js
@@ -55,10 +55,13 @@ const age = 42;
 
 //　従来の方法
 const message1 = "私の名前は" + name + "です。" + "年齢は" + age + "です。";
+console.log("------------------------");
 console.log(message1);
 
 // テンプレート文字列を用いた方法
+console.log("------------------------");
 console.log("テンプレート文字列を用いた方法:");
 const message2 = `私の名前は${name}です。年齢は${age}です。`;
 console.log(message2);
+
 
